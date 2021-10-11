33a18aea20f68b6e8db7c2134ae59ecdd8e36190
Mia Stroud <miast36@gmail.com>
2021-10-11 09:40:39 -0400

    Fix typos

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
     <div className="App">
       <Navbar/>
       <div className="newsletter-area">
-        <h4>Grief is hard. We can help</h4>
+        <h4>Grief is hard. We can help.</h4>
         <button className="join-newsletter-btn" onClick={() => setformPopup(true)}>Join Newsletter</button>
       </div>
       {formPopup && 
@@ -36,7 +36,7 @@ function App() {
           <div className="service">
             <div className="img-container"><img src={Phone} alt="" className="info-image" /></div>
             <p className="service-title">Mobile Ecosystem</p>
-            <p className="service-description">Our mobile application acts as a predictive engine to surface the "next best step" in your grief journey, coupled with prompts for reflection, activity, and response</p>
+            <p className="service-description">Our mobile application acts as a predictive engine to surface the "next best step" in your grief journey, coupled with prompts for reflection, activity, and response.</p>
           </div>
           <div className="service">
             <div className="img-container"><img src={CompanionIcon} alt="" className="info-image" /></div>
@@ -61,7 +61,7 @@ function App() {
       {/* <Testimonials /> */}
       <div className="newsletter-section">
         <h3 className="section-title" id="newsletter-signup">Newsletter Sign Up</h3>
-        <p>Sign up for the newseletter for a weekly dose of inspiration and ways to connect with our community</p>
+        <p>Sign up for the newsletter for a weekly dose of inspiration and ways to connect with our community</p>
         <MailchimpForm formId="page-form" />
       </div>
       <Footer />
