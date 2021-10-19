c93322bbb9c1a58165baf61f6ab1b343570db777
Mia Stroud <miast36@gmail.com>
2021-10-19 18:30:46 -0400

    Added gtm manager and gtm tag

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './App';
+import TagManager from 'react-gtm-module';
+
+const tagManagerArgs = {
+  gtmId: 'GTM-TCJN7J5'
+}
+
+TagManager.initialize(tagManagerArgs)
 
 ReactDOM.render(
   <React.StrictMode>
